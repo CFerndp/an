45ee544d8bff2ef35a4feaee25836e75292e9d86
Cristian Fernandez <cristian.fernandez.del.pozo@gmail.com>
2020-12-27 16:35:36 +0100

    Added parse level

--- models/games/Level.ts
+++ models/games/Level.ts
@@ -1,1 +1,13 @@
 export type Level = "easy" | "medium" | "hard";
+
+export const parseLevel = (level: string) => {
+  const handleString = level.toLowerCase().trim();
+
+  if (handleString === "hard") {
+    return "hard" as Level;
+  } else if (handleString === "medium") {
+    return "medium" as Level;
+  } else {
+    return "easy" as Level;
+  }
+};
